a25304f1131d9b0b879d74cd0b2f0a71aded6f95
Alexander Lutsenko <alexxx.lutsenko@gmail.com>
2026-06-12 16:51:53 +0300

    feat: set up a basic footer

--- src/3_widgets/footer-widget/ui/FooterWidget.tsx
+++ src/3_widgets/footer-widget/ui/FooterWidget.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const FooterWidget: React.FC = () => {
   return (
     <footer>
-      <div></div>
+      <div className="px-10 py-5 text-center">
+        <span className="text-text-secondary text-sm font-bold">{`@${new Date().getFullYear()} SpaceHub. All rights reserved.`}</span>
+      </div>
     </footer>
   );
 };
