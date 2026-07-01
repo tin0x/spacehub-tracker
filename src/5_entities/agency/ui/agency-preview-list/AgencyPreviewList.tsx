ad8536806c190567dec3b51827dcc385794369a9
Alexander Lutsenko <alexxx.lutsenko@gmail.com>
2026-07-01 16:35:39 +0300

    feat: adapt the agency preview list for different devices

--- src/5_entities/agency/ui/agency-preview-list/AgencyPreviewList.tsx
+++ src/5_entities/agency/ui/agency-preview-list/AgencyPreviewList.tsx
@@ -4,7 +4,7 @@ import AgencyPreviewItem from '@entities/agency/ui/agency-preview-item/AgencyPre
 
 const AgencyPreviewList: React.FC<AgencyPreviewListProps> = ({ agencies }) => {
   return (
-    <ul className="grid grid-cols-3 gap-5">
+    <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
       {agencies?.map((agency) => (
         <li key={agency.id}>
           <AgencyPreviewItem
